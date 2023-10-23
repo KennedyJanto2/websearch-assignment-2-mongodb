b803070e00e0bfdce9cccdec1f279ac9582a2107
KennedyJanto2 <Kennedyjanto3@gmail.com>
2023-10-23 01:36:03 -0700

    finished assignment

---
+++ db_connection_mongo.py
@@ -0,0 +1,90 @@
+#-------------------------------------------------------------------------
+# AUTHOR: your name
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4250- Assignment #2
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with
+# standard arrays
+
+#importing some Python libraries
+from pymongo import MongoClient
+
+def connectDataBase():
+
+    # Create a database connection object using pymongo
+    client = MongoClient('localhost', 27017)
+    db = client.corpus
+    col = db.documents
+    return db, col
+
+def createDocument(col, docId, docText, docTitle, docDate, docCat):
+
+    # create a dictionary to count how many times each term appears in the document.
+    # Use space " " as the delimiter character for terms and remember to lowercase them.
+    term_count = {}
+    words = docText.split()
+    for word in words:
+        word = word.lower()
+        if word not in term_count:
+            term_count[word] = 1
+        else:
+            term_count[word] += 1
+
+    # create a list of dictionaries to include term objects.
+    term_objects = [{"term": k, "count": v} for k, v in term_count.items()]
+
+    #Producing a final document as a dictionary including all the required document fields
+    doc = {
+        "_id": docId,
+        "text": docText,
+        "title": docTitle,
+        "date": docDate,
+        "category": docCat,
+        "terms": term_objects
+    }
+
+    # Insert the document
+    col.insert_one(doc)
+
+def deleteDocument(col, docId):
+
+    # Delete the document from the database
+    col.delete_one({"_id": docId})
+
+def updateDocument(col, docId, docText, docTitle, docDate, docCat):
+
+    # Delete the document
+    deleteDocument(col, docId)
+
+    # Create the document with the same id
+    createDocument(col, docId, docText, docTitle, docDate, docCat)
+
+def getIndex(col):
+
+    # Query the database to return the documents where each term occurs with their corresponding count. Output example:
+    # {'baseball':'Exercise:1','summer':'Exercise:1,California:1,Arizona:1','months':'Exercise:1,Discovery:3'}
+    # ...
+    inverted_index = {}
+    cursor = col.find({})
+
+    for document in cursor:
+        for term_obj in document["terms"]:
+            term = term_obj["term"]
+            count = term_obj["count"]
+            title = document["title"]
+            
+            if term not in inverted_index:
+                inverted_index[term] = {}
+            
+            inverted_index[term][title] = count
+
+    # Formatting the output as per the given example
+    output_index = {}
+    for term, titles in inverted_index.items():
+        output_str = ",".join([f"{title}:{count}" for title, count in titles.items()])
+        output_index[term] = output_str
+
+    return output_index
--- index.py
+++ index.py
@@ -7,20 +7,16 @@
 #-----------------------------------------------------------*/
 
 #importing some Python libraries
-from db_connection import *
+from db_connection_mongo import *
 
 if __name__ == '__main__':
 
     # Connecting to the database
-    conn = connectDataBase()
-
-    # Getting a cursor
-    cur = conn.cursor()
+    db, col = connectDataBase()
 
     #print a menu
     print("")
     print("######### Menu ##############")
-    print("#a - Create a category.")
     print("#b - Create a document")
     print("#c - Update a document")
     print("#d - Delete a document.")
@@ -33,15 +29,7 @@ if __name__ == '__main__':
           print("")
           option = input("Enter a menu choice: ")
 
-          if (option == "a"):
-
-              catId = input("Enter the ID of the category: ")
-              catName = input("Enter the name of the category: ")
-
-              createCategory(cur, catId, catName)
-              conn.commit()
-
-          elif (option == "b"):
+          if (option == "b"):
 
               docId = input("Enter the ID of the document: ")
               docText = input("Enter the text of the document: ")
@@ -49,8 +37,7 @@ if __name__ == '__main__':
               docDate = input("Enter the date of the document: ")
               docCat = input("Enter the category of the document: ")
 
-              createDocument(cur, docId, docText, docTitle, docDate, docCat)
-              conn.commit()
+              createDocument(col, docId, docText, docTitle, docDate, docCat)
 
           elif (option == "c"):
 
@@ -60,21 +47,17 @@ if __name__ == '__main__':
               docDate = input("Enter the date of the document: ")
               docCat = input("Enter the category of the document: ")
 
-              updateDocument(cur, docId, docText, docTitle, docDate, docCat)
-
-              conn.commit()
+              updateDocument(col, docId, docText, docTitle, docDate, docCat)
 
           elif (option == "d"):
 
               docId = input("Enter the document id to be deleted: ")
 
-              deleteDocument(cur, docId)
-
-              conn.commit()
+              deleteDocument(col, docId)
 
           elif (option == "e"):
 
-              index = getIndex(cur)
+              index = getIndex(col)
               print(index)
 
           elif (option == "q"):
